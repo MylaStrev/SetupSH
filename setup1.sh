557e6d057ab3bbc8cb7037611049f68bb0fa93dc
myla_00 <74766120+MylaStrev@users.noreply.github.com>
2025-09-20 01:11:33 +0100

    Update setup1.sh
    add flatpak and flathub 
    add package list for flatpak and flathub

--- setup1.sh
+++ setup1.sh
@@ -14,6 +14,16 @@ APT_PACKAGES=(
 "git"
 )
 
+FLATPAK_PAKAGES=(
+"com.valvesoftware.Steam"
+"md.obsidian.Obsidian"
+)
+
+FLATHUB_PACKAGE=(
+"com.spotify.client"
+"com.discordapp.Discord"
+)
+
 SNAP_PACKAGES=(
 "spotify"
 "steam"
@@ -30,6 +40,13 @@ if test -f "/etc/apt/preferences.d/nosnap.pref"; then
     echo "## file removed"
 else
     echo "## file not found"
+fi
+
+#install flathub repo
+echo "## checking for and getting flathub repo..."
+if ! flatpak remote-list | grep -q flathub; then
+    echo "## Adding Flathub remote..."
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
 
 # check for and install snap
@@ -70,6 +87,26 @@ for pkg in "${SNAP_CLASSIC_PACKAGES[@]}"; do
     else
         echo "## installing $pkg via snap --classic"
         sudo snap install "$pkg" --classic
+    fi
+done
+
+echo "## INSTALLING FLATPAK PACKAGES"
+for pkg in "${FLATPAK_PLACKAGES[@]}"; do
+    if dpkg -s "$pkg" &> /dev/null; then
+        echo "## $pkg is already installed, skipping..."
+    else 
+        echo "## installing $pkg..."
+        sudo flatpak install -y "$pkg"
+    fi
+done
+
+echo "## INSTALLING FLATHUB PACKAGES"
+for pkg in "${FLATHUB_PLACKAGES[@]}"; do
+    if dpkg -s "$pkg" &> /dev/null; then
+        echo "## $pkg is already installed, skipping..."
+    else 
+        echo "## installing $pkg..."
+        sudo flatpak install -y flathub "$pkg"
     fi
 done
 
